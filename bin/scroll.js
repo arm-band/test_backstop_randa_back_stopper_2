fd401e00b0b18db78a7e20ab1935d44ca691c1ff
arm-band <bre4tn.let@gmail.com>
2020-12-20 00:30:08 +0900

    スクロールだけにして、もう少し細かくconsole.logを出すように

--- bin/scroll.js
+++ bin/scroll.js
@@ -1,9 +1,4 @@
-const puppeteer = require('puppeteer');
-const merge = require('merge-img');
-
-async function scrollToBottom (page, viewport) {
-    await page.screenshot({path: 'tmp/fullscreen.png', fullPage: true});
-
+async function scrollToBottom (page, viewport, scenario) {
     const getScrollHeight = () => {
         return Promise.resolve(document.documentElement.scrollHeight);
     }
@@ -12,18 +7,14 @@ async function scrollToBottom (page, viewport) {
     let currentPosition = 0;
     let scrollNumber = 0;
 
-    let screens = [];
     while (currentPosition < scrollHeight) {
-        console.log("Screen "+scrollNumber);
-        let screen = await page.screenshot({path: 'tmp/screen-'+scrollNumber+'.png', fullPage: false, clip: {x: 0, y:scrollNumber*viewport.height, width: viewport.width, height: viewport.height}});
-        screens.push(screen);
         scrollNumber += 1;
         const nextPosition = scrollNumber * viewport.height;
         await page.evaluate(function (scrollTo) {
             return Promise.resolve(window.scrollTo(0, scrollTo))
         }, nextPosition);
         await page.waitForNavigation({waitUntil: 'networkidle2', timeout: 10000})
-                .catch(e => console.log('timeout exceed. proceed to next operation'));
+                .catch(e => console.log(`scrolling ${scenario.label}-${viewport.name}-${scrollNumber}: timeout exceed. proceed to next operation`));
 
         currentPosition = nextPosition;
         console.log(`scrollNumber: ${scrollNumber}`);
@@ -32,29 +23,20 @@ async function scrollToBottom (page, viewport) {
         scrollHeight = await page.evaluate(getScrollHeight);
         console.log(`ScrollHeight ${scrollHeight}`);
     }
-    img = await merge(screens, {direction: true, offset: 20, margin: 10, color: 0xFF0000FF});
-    img.write('tmp/out.png');
 };
 
 module.exports = async (page, scenario, vp) => {
     console.log('SCENARIO > ' + scenario.label);
     await require('../backstop_data/engine_scripts/puppet/clickAndHoverHelper')(page, scenario);
-    const vpw = vp.width;
-    const vph = vp.height;
-    const browser = await puppeteer.launch();
-    page = await browser.newPage();
 
-    await page.goto(scenario.url);
-    await page.setViewport({width: vpw, height: vph});
+    // added
+    await page.waitForNavigation({waitUntil: 'networkidle2', timeout: 10000})
+            .catch(e => console.log('before scroll:: timeout exceed. proceed to next operation'));
 
     const $ele = await page.$('body');
     const { width, height } = await $ele.boundingBox();
-    console.log("W="+width+" H="+height);
-//    await page.waitForNavigation({'waitUntil' : 'networkidle'});
-    await page.waitForNavigation({waitUntil: 'networkidle2', timeout: 10000})
-            .catch(e => console.log('timeout exceed. proceed to next operation'));
+    console.log(`${scenario.label}-${vp.name}: w=${width} h=${height}`);
 
-    await scrollToBottom(page, vp);
 
-    await browser.close();
+    await scrollToBottom(page, vp, scenario);
 };
