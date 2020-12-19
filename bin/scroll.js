22e8aa46ce72a2a4793582f5d7887f72d91a2367
arm-band <bre4tn.let@gmail.com>
2020-12-20 00:22:07 +0900

    軽率にスクロール処理を合体させてみた

--- bin/scroll.js
+++ bin/scroll.js
@@ -1,6 +1,33 @@
 const puppeteer = require('puppeteer');
 const merge = require('merge-img');
 
+async function scrollToBottom (page, viewportHeight) {
+    const getScrollHeight = () => {
+        return Promise.resolve(document.documentElement.scrollHeight);
+    }
+
+    let scrollHeight = await page.evaluate(getScrollHeight);
+    let currentPosition = 0;
+    let scrollNumber = 0;
+
+    while (currentPosition < scrollHeight) {
+        scrollNumber += 1;
+        const nextPosition = scrollNumber * viewportHeight;
+        await page.evaluate(function (scrollTo) {
+            return Promise.resolve(window.scrollTo(0, scrollTo))
+        }, nextPosition);
+        await page.waitForNavigation({waitUntil: 'networkidle2', timeout: 10000})
+                .catch(e => console.log('timeout exceed. proceed to next operation'));
+
+        currentPosition = nextPosition;
+        console.log(`scrollNumber: ${scrollNumber}`);
+        console.log(`currentPosition: ${currentPosition}`);
+
+        scrollHeight = await page.evaluate(getScrollHeight);
+        console.log(`ScrollHeight ${scrollHeight}`);
+    }
+};
+
 module.exports = async (page, scenario, vp) => {
     console.log('SCENARIO > ' + scenario.label);
     await require('../backstop_data/engine_scripts/puppet/clickAndHoverHelper')(page, scenario);
@@ -16,6 +43,11 @@ module.exports = async (page, scenario, vp) => {
     const { width, height } = await $ele.boundingBox();
     console.log("W="+width+" H="+height);
 //    await page.waitForNavigation({'waitUntil' : 'networkidle'});
+    await page.waitForNavigation({waitUntil: 'networkidle2', timeout: 5000})
+            .catch(e => console.log('timeout exceed. proceed to next operation'));
+
+    await scrollToBottom(page, vp.height);
+
 
     await page.screenshot({path: 'tmp/fullscreen.png', fullPage: true});
 
